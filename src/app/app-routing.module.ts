e8a40b2dea1951b253db783aef51877290dc9905
Gerardo Velez <gerardo.velez.sanchez@gmail.com>
2020-06-10 19:36:43 -0500

    OneSinglePage approach

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ArticulosComponent } from './pages/articulos/articulos.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  },
-  {
-    path: 'articulos',
-    component: ArticulosComponent
   },
   {
     path: '**',
@@ -24,5 +19,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent, ArticulosComponent];
+export const routingComponents = [HomeComponent];
 
